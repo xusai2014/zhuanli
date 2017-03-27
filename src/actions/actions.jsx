934286d333093bdcc6b68feb2160ed8b05fbb1fe
jerryxu17235 <13048699388@163.com>
2017-03-28 01:13:09 +0800

    Dispatch Promise

--- src/actions/actions.jsx
+++ src/actions/actions.jsx
@@ -1,6 +1,26 @@
+
 export function setNameActionCreator (name) {
     return {
         type: 'SET_NAME',
         name: name
     }
+}
+
+export function zhuanliSearch(data) {
+    return{
+        types:['GET_Zhuanli_Search_REQUEST', 'GET_Zhuanli_Search_SUCCESS', 'GET_Zhuanli_Search_FAILURE'],
+        data:data,
+        promise:()=>{
+            return new Promise((resolve, reject) => {
+                var req = new Request(URL, {method: 'GET', data: data});
+                fetch(req).then(function(response) {
+                    var body = response.body
+                    resolve({data:body})
+                }).catch(function(err) {
+                    reject({err:err})
+                });
+            })
+        }
+    }
+
 }
--- src/components/pages/SimpleSearch.jsx
+++ src/components/pages/SimpleSearch.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import A from '../modules/ZhuanliList'
+import {zhuanliSearch} from '../../actions/actions'
 
 class SimpleSearch extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            china:true,
+            world:false
+        }
+    }
+    selectRadio(e){
+           if(e.currentTarget.value === "china") {
+               this.setState({
+                   china:true,
+                   world:false
+               })
+           } else {
+               this.setState({
+                   china:false,
+                   world:true
+               })
+           }
+    }
+    zhuanliSearch(){
+        const data ={}
+        debugger
+        this.props.dispatch(zhuanliSearch(data))
+    }
+
     render(){
         return( <div>
             <A/>
@@ -12,15 +39,15 @@ class SimpleSearch extends React.Component{
                       <div className="input-group-btn"><span className="form-control textwidth">专利检索:</span></div>
                         <input type="text" className="form-control" placeholder="专利检索,请输入检索关键词例如：计算机" />
                             <span className="input-group-btn">
-                                <button className="btn btn-default" type="button">检索
+                                <button className="btn btn-default" type="button" onClick={()=>this.zhuanliSearch()}>检索
                                     <span className="glyphicon glyphicon-search" aria-hidden="true"></span></button>
                               </span>
                         </div>
                 </div>
                 <div className="row marginRow">
                     <div className="checkboxRow">
-                            <div  className="textwidth" ><input type="checkbox" aria-label="..."  />中国专利</div>
-                            <div  className="textwidth" ><input type="checkbox" aria-label="..." />世界专利</div>
+                            <div  className="textwidth" ><input type="radio" value="china" checked={this.state.china} aria-label="..."  onChange={(e)=> this.selectRadio(e)}/>中国专利</div>
+                            <div  className="textwidth" ><input type="radio" value="world" checked={this.state.world} aria-label="..." onChange={(e)=> this.selectRadio(e)}/>世界专利</div>
 
                     </div>
                 </div>
